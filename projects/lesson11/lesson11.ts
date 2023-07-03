ed6b436082f101db01c72e574d976431304a8b61
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-03 15:53:55 +0700

    Lesson 11 - Object Type

---
+++ projects/lesson11/lesson11.ts
@@ -0,0 +1,12 @@
+let proLesson11: {
+  name: string;
+  age: number;
+} = {
+  name: "dat",
+  age: 23,
+};
+
+// proLesson11.address = "asdasd";
+// address is not in proLesson11
+
+console.log(proLesson11);
