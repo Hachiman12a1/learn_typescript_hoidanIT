f2eedb4cadda279ea164f4a902fe41f3b36d8e3e
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-04 14:41:17 +0700

    Lesson 21 - Switch Case

---
+++ projects/Chapter3/lesson21.ts
@@ -0,0 +1,14 @@
+let age: number = 20;
+
+switch (age) {
+  case 20:
+  case 19:
+    console.log("Đã đi làm");
+    break;
+  case 12:
+    console.log("Đã đi làm sinh viên");
+    break;
+  default:
+    console.log(">>> Run Default");
+    break;
+}
