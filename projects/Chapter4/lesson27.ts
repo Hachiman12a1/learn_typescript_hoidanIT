9718d4bf073741da59b9fbe4e5474669fe9784e2
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-05 09:36:17 +0700

    Lesson 27 - Functions

---
+++ projects/Chapter4/lesson27.ts
@@ -0,0 +1,10 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+//anonymous function
+const sum2 = (a: number, b: number) => {
+  return a + b;
+};
+
+console.log(">>> check sum: ", sum2(6, 9));
