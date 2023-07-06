a432e8fede137284119d02e3510dd9ef0926c5ad
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-06 14:22:15 +0700

    Lesson 38 - Static Methods And Properties

---
+++ projects/Chapter4/lesson38.ts
@@ -0,0 +1,10 @@
+class Circle {
+  static pi: number = 3.14;
+  public test: number = 69;
+  static calculateArea(radius: number) {
+    return this.pi * radius * radius;
+  }
+}
+
+let t = new Circle();
+console.log(">>> check pi= ", Circle.calculateArea(10));
