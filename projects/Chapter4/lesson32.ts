3d43afa12ef4656a72a4aa84bb29b0fe7eae8c3e
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-07 15:50:06 +0700

    Revert "Lesson 32 - Type Function Loading"
    This reverts commit 92db2bd499372c745cd896c85454687e5874d3e1.

--- projects/Chapter4/lesson32.ts
+++
@@ -1,9 +0,0 @@
-// overloading
-function addNew(a: number, b: number): number;
-function addNew(a: string, b: string): string;
-
-function addNew(a: any, b: any) {
-  return a + b;
-}
-
-console.log(addNew(1, 2), addNew("Dao", " Van Dat"));
